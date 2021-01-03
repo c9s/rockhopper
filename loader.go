fb31524d61b466ada7e1a86fc328337066ced310
c9s <yoan@maicoin.com>
2021-01-03 12:13:37 +0800

    add rockhopper db open function with sqlite3

---
+++ loader.go
@@ -0,0 +1,41 @@
+package rockhopper
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type DB struct {
+	*sql.DB
+
+	dialect SQLDialect
+}
+
+// Open creates a connection to a database
+func Open(driverName string, dbstring string, dialect SQLDialect) (*DB, error) {
+	switch driverName {
+	case "mssql":
+		driverName = "sqlserver"
+	case "redshift":
+		driverName = "postgres"
+	case "tidb":
+		driverName = "mysql"
+	}
+
+	switch driverName {
+	// supported drivers
+	case "postgres", "sqlite3", "mysql", "sqlserver":
+	default:
+		return nil, fmt.Errorf("unsupported driver %s", driverName)
+	}
+
+	db, err := sql.Open(driverName, dbstring)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{
+		dialect: dialect,
+		DB: db,
+	}, nil
+}
---
+++ loader_test.go
@@ -0,0 +1,21 @@
+package rockhopper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func TestOpen(t *testing.T) {
+	dialect, err := LoadDialect("sqlite3")
+	assert.NoError(t, err)
+
+	db, err := Open("sqlite3", ":memory:", dialect)
+	assert.NoError(t, err)
+	assert.NotNil(t, db)
+
+	err = db.Close()
+	assert.NoError(t, err)
+}
