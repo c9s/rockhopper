2be8da91cf6d7d697f9a107e175b7f0cd9b50417
c9s <yoan@maicoin.com>
2021-01-09 22:06:37 +0800

    add driver files for mysql, pgsql and sqlite3

---
+++ driver_pgsql.go
@@ -0,0 +1,7 @@
+// +build !no_postgres
+
+package rockhopper
+
+import (
+	_ "github.com/lib/pq"
+)
---
+++ driver_sqlite3.go
@@ -0,0 +1,7 @@
+// +build !no_sqlite3
+
+package rockhopper
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+)
